8174a832d84ef2c338876cc7104cd49d0080b376
hema_varshini_b <hemavarshinius@gmail.com>
2025-07-15 23:10:10 -0400

    removed unnecessary deps

--- app/routes/webhooks.app.scopes_update.tsx
+++ app/routes/webhooks.app.scopes_update.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { shopify } from "../shopify.server";
-import db from "../db.server";
+// import db from "../db.server";
 
 export const action = async ({ request, context }: ActionFunctionArgs) => {
     const { payload, session, topic, shop } = await shopify(context).authenticate.webhook(request);
@@ -8,14 +8,14 @@ export const action = async ({ request, context }: ActionFunctionArgs) => {
 
     const current = payload.current as string[];
     if (session) {
-        await db(context.cloudflare.env.DATABASE_URL).session.update({   
-            where: {
-                id: session.id
-            },
-            data: {
-                scope: current.toString(),
-            },
-        });
+        // await db(context.cloudflare.env.DATABASE_URL).session.update({   
+        //     where: {
+        //         id: session.id
+        //     },
+        //     data: {
+        //         scope: current.toString(),
+        //     },
+        // });
     }
     return new Response();
 };
--- app/routes/webhooks.app.uninstalled.tsx
+++ app/routes/webhooks.app.uninstalled.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { shopify } from "../shopify.server";
-import db from "../db.server";
+// import db from "../db.server";
 
 export const action = async ({ request, context }: ActionFunctionArgs) => {
   const { shop, session, topic } = await shopify(context).authenticate.webhook(request);
@@ -10,7 +10,7 @@ export const action = async ({ request, context }: ActionFunctionArgs) => {
   // Webhook requests can trigger multiple times and after an app has already been uninstalled.
   // If this webhook already ran, the session may have been deleted previously.
   if (session) {
-    await db(context.cloudflare.env.DATABASE_URL).session.deleteMany({ where: { shop } });
+    // await db(context.cloudflare.env.DATABASE_URL).session.deleteMany({ where: { shop } });
   }
 
   return new Response();
